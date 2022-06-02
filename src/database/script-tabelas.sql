8a2231a48274998aaa9a93e16bd376d503224f09
MBisacchi <mayla.bisacchi@sptech.school>
2022-06-02 14:26:27 -0300

    Incorporação do db na api

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,60 +1,43 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+-- Script: Projeto Individual 
+-- SkinYoung
 
-/* para workbench - local - desenvolvimento */
-CREATE DATABASE acquatec;
+-- Criação do banco de dados:
+create database skinyoung;
 
-USE acquatec;
+-- Usar banco de dados:
+use skinyoung;
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50)
+-- Criação da tabela 'Usuário':
+create table usuario (
+idUsuario int primary key auto_increment,
+nome varchar (70),
+email varchar (70),
+dtNasc date,
+senha varchar (45)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-    descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-); 
-
-CREATE TABLE medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	temperatura DECIMAL,
-	umidade DECIMAL,
-	momento DATETIME,
-	fk_aquario INT
+-- Criação da tabela 'Alerta':
+create table alerta (
+idAlerta int primary key auto_increment,
+tipoAlerta varchar (45),
+dtAlerta date,
+fkUsuario int,
+foreign key (fkUsuario) references usuario(idUsuario)
 );
 
-
-
-
-/* para sql server - remoto - produção */
-
-CREATE TABLE usuario (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
+-- Criação da tabela 'Quiz':
+create table quiz (
+idQuiz int primary key auto_increment,
+nome varchar (45)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	titulo VARCHAR(100),
-    descricao VARCHAR(150),
-	fk_usuario INT FOREIGN KEY REFERENCES usuario(id)
-); 
-
-CREATE TABLE medida (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	temperatura DECIMAL,
-	umidade DECIMAL,
-	momento DATETIME,
-	fk_aquario INT
+-- Criação da tabela 'Relatório':
+create table relatorio (
+fkUsuario int,
+foreign key (fkUsuario) references usuario(idUsuario),
+fkQuiz int,
+foreign key (fkQuiz) references quiz(idQuiz),
+primary key (fkUsuario, fkQuiz),
+dtQuiz datetime default current_timestamp,
+resultado varchar (45)
 );
-
-
